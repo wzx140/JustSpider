78a3ab4ca56a9fc8448032f6c0006bdc331b248f
wzx <siwangqishi1997@sina.cn>
2017-07-27 20:52:26 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 7/27/17 9:35 AM
+# @Author  : WZX
+# @Site    : 
+# @File    : config.py
+# @Software: PyCharm
+
+# 用户名
+user_name = ''
+
+# 密码
+password = ''
+
+# 不需修改
+five_to_grade = {'优': 4.5, '良': 3.5, '中': 2.5, '及格': 1.5, '不及格': 0}
+
+# 学期 如2015-2016-2等
+time = '2015-2016-1'
